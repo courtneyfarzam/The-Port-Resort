3eaa4e40c192349926aafb1b3799d4365bec72b1
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-11-07 22:56:18 -0600

    worked on controllers needs views to test the routes

--- controllers/dashboardRoute.js
+++ controllers/dashboardRoute.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
-const { Portfolio, User } = require('../../models');
-const withAuth = require('../../utils/auth');
-
+const { Portfolio, User } = require('../models');
+const withAuth = require('../utils/auth');
+//wont work unless logged in 
 router.get('/', withAuth, async (req, res) => {
     try {
         const portfolioData = await Portfolio.findAll ({
-            where: {user_id: req.session.user_id},
             attributes: [
                 'id', 
                 'full_name', 
@@ -37,18 +36,64 @@ router.get('/', withAuth, async (req, res) => {
                 }
             ]
         });
-
-         const portfolio = portfolioData.map(post => post.get({ plain: true }));
+//for the render of page
+         const Portfolio = portfolioData.map(post => post.get({ plain: true }));
+        res.json(portfolioData)
     } catch(err) {
-        res.status(500).json(err);
+        res.status(500).json({messgae: 'check line 43 dbr'});
     }
 });
 
 router.get('/edit/:id', withAuth, async (req, res) => {
     try {
+        const portfolioData = await Portfolio.findOne ({
+                 where: {
+                    id: req.body.user_id
+                 },
+                 include: [
+                    {
+                        model: Portfolio,
+                        attributes: [
+                            'id', 
+                            'full_name', 
+                            'job_title', 
+                            'about',
+                            'phone',
+                            'country',
+                            'city',
+                            'linkedin',
+                            'github',
+                            'school',
+                            'course',
+                            'graduation_date',
+                            'role',
+                            'company',
+                            'location',
+                            'job_starting',
+                            'job_ending',
+                            'job_description',
+                            'project_name',
+                            'repository',
+                            'project_description'
+                        ]
+                    }
+                 ],
+                 include: [
+                    {
+                        model: User,
+                        attributes: ['name']
+                    }
+                 ]
+        });
 
+        if(!portfolioData) {
+            res.status(400).json({message: 'No poertfolio to edit'});
+            return;
+        }
+
+        res.status(200).json(portfolioData)
     } catch(err) {
-        res.status(500).json(err);
+        res.status(500).json({message: 'check line 96 dbr'});
     }
 });
 
--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,30 +1,63 @@
-// const router = require('express').Router();
-// const { User, Portfolio } = require('../../models');
-// const withAuth = require('../../utils/auth');
+const router = require('express').Router();
+const { User, Portfolio } = require('../models');
+// const withAuth = require('../utils/auth');
 
-// router.get('/', async (req, res) => {
-//     try {
+router.get('/', async (req, res) => {
+    try {
+        const portfolioData = await Portfolio.findAll ({
+            attributes: [
+                'id', 
+                'full_name', 
+                'job_title', 
+                'about',
+                'phone',
+                'country',
+                'city',
+                'linkedin',
+                'github',
+                'school',
+                'course',
+                'graduation_date',
+                'role',
+                'company',
+                'location',
+                'job_starting',
+                'job_ending',
+                'job_description',
+                'project_name',
+                'repository',
+                'project_description'
+            ],
 
-//     } catch(err) {
-//         res.status(500).json(err);
-//     }
-// });
+            include: [
+                {
+                    model: User,
+                    attributes: ['name']
+                }
+            ]
+        });
+res.json(portfolioData)
+    } catch(err) {
+        res.status(500).json({message: 'check line 43 hr'});
+    }
+});
 
-// router.get('/login', async (req, res) => {
-//     try {
+//we need to add in render for login page :)
+router.get('/login', async (req, res) => {
+   if(req.session.logged_in) {
+    res.redirect('/profile')
+    return;
+   }
+});
 
-//     } catch(err) {
-//         res.status(500).json(err);
-//     }
-// });
 
-// router.get('/signup', async (req, res) => {
-//     try {
+router.get('/signup', async (req, res) => {
+    try {
 
-//     } catch(err) {
-//         res.status(500).json(err);
-//     }
-// });
+    } catch(err) {
+        res.status(500).json({message: 'check line 43 hr'});
+    }
+});
 
 // router.get('/portfolio/:id', async (req, res) => {
 //     try {
@@ -34,4 +67,4 @@
 //     }
 // });
 
-// module.exports = router;
+module.exports = router;
